ba7262e9319fec31a8f6248a678b9eed9b489bb1
Karthi Keya.Y <karthicky45@gmail.com>
2026-09-11 11:46:37 +0530

    EX-1

--- EBBillCalculation.java
+++ EBBillCalculation.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class EBBillCalculation {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         int consumerNo, previousReading, currentReading, units;
